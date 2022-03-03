e12433637419efb7a6a6d5fa2e724b3d6f6b7582
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-03-03 13:12:37 -0500

    Created Champs vue
    Started the champ page

---
+++ project/src/views/Champs.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>League of legends Champions</div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "League Champs"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

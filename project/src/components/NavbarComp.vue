63cada5fb82f8049d4c20f578d57c72fae537793
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-03-04 22:28:30 -0500

    Updated NavBar
    Gave NavBar text decorations, hover effects, and black & gold color scheme.

--- project/src/components/NavbarComp.vue
+++ project/src/components/NavbarComp.vue
@@ -1,18 +1,25 @@
 <template>
 	<nav>
-		<template v-if="authIsReady">
-			<div>
-				<router-link to="/">Home</router-link> | 
-				<router-link to="search">Search</router-link>
-			</div>
-			<div v-if="user">
-				<router-link to="user">User</router-link> | 
-				<span>Logged in as {{ user.email }}</span> | 
-				<button @click="handleClick">Logout</button>
-			</div>
-			<div v-if="!user">
-				<router-link to="signup">Sign Up</router-link> |
-				<router-link to="login">Login</router-link>
+		<template v-if="authIsReady" >
+			<div class="navbar">
+
+				<div class="left">
+					<router-link to="/">Home</router-link> | 
+					<router-link to="search">Search</router-link>
+					
+			
+				</div>
+				<div v-if="user" class="right">
+					<router-link to="user">User</router-link> | 
+					<span>Logged in as {{ user.email }}</span> | 
+					<button @click="handleClick">Logout</button>
+				</div>
+				<div v-if="!user" class="right">
+					<router-link to="signup">Sign Up</router-link> |
+					<router-link to="login">Login</router-link>
+				</div>
+
+				
 			</div>
 		</template>
 	</nav>
@@ -38,3 +45,47 @@ export default {
 	},
 };
 </script>
+<style scoped>
+	#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+
+
+.navbar{
+background-color: black;
+ margin-top: 2rem;
+
+ display:flex;
+ align-items: center;
+ padding: 3rem;
+ justify-content: space-around;
+}
+
+
+nav a {
+	padding: 3rem;
+	font-weight: bold;
+	color: #FFF;
+	font-size: 2rem;
+
+	transition: 500ms;
+	text-decoration: underline ;
+	text-decoration-color: transparent;
+
+	
+	
+}
+nav a:hover {
+	color: #FFD700;
+	text-decoration-color: #FFD700;
+}
+
+nav a.router-link-exact-active {
+	color: #FFD700;
+	text-decoration-color: #FFD700;
+}
+</style>

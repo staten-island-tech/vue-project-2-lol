cf404b5462861e6e176df7d9944c238156d81547
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-03-09 13:12:39 -0500

    creating cards

---
+++ project/src/components/ChampCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="asdf">
+        <img class='img' :src={ImgSource} alt=""/>
+        <h2 class="name">{{ChampName}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChampCard",
+    props:{
+        ImgSource:String,
+        ChampName:String,
+    }
+}
+</script>
+<style scoped>
+    .asdf{
+        background-color: yellow;
+    }
+</style>
--- project/src/views/ChampionsView.vue
+++ project/src/views/ChampionsView.vue
@@ -3,20 +3,30 @@
         This is a test
         <button @click="handleClicked">Test</button>
         
-        <h1 v-for="Champion in DataList" :key="Champion.name">{{Champion.id}}</h1>
+        <div v-for="champion in DataList" :key="champion.name" class="container">
+            <ChampCard 
+                :ChampName="champion.name"
+               :ImgSource= {{getIMG(champion.name)}}
+                
+             
+            />
+
+        </div>
     </div>
 </template>
 
 <script>
 import {useStore } from "vuex";
-import ChampData from "../ddragon/en_US/champion.json"
+import ChampData from "../ddragon/en_US/championFull.json"
+import ChampCard from "../components/ChampCard.vue"
+
     export default {
         setup(){
             const store = useStore()
 
             const handleClicked =  function(){
                 console.log("Hello")}
-         
+        
             const DataList = ChampData.data
             return{
                 users: ChampData,
@@ -25,9 +35,14 @@ import ChampData from "../ddragon/en_US/champion.json"
             }
         
         },
-        methods:{
-            
-        },
+        components:{ChampCard},
+        computed:{
+            getIMG: function(champ){
+                return `../ddragon/img/champion/${champ}.png`
+
+            }
+        }
+
         
     }
 </script>

41dd1be2ad7f7ed66ec5fd8456b27d744b59dd6d
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-04-02 22:03:46 -0400

    Updated ChampParams

--- project/src/views/ChampParams.vue
+++ project/src/views/ChampParams.vue
@@ -1,8 +1,18 @@
 <template>
-    <div>
-        <h2 class="title">{{this.$route.params.name}}</h2>
-        <h2 class="title">{{this.Results}}</h2>
-        <img :src="'https://ddragon.leagueoflegends.com/cdn/11.24.1/img/champion/' + this.$route.params.name + '.png'" alt="">
+    <div class="page" >
+        <h2 class="name">{{this.Results.title}}</h2>
+        <h2 class="name">{{this.Results.name}}</h2>
+        <div class="BG">
+            <img class="splashImg" :src="getSplash" alt="">
+            <img class="blurBG" :src="getSplash" alt="">
+        </div>
+       
+     
+        <h2 class="title">{{this.Results.lore}}</h2>
+        <h2 class="title">{{this.Results.allytips}}</h2>
+        <h2 class="title">{{this.Results.enemytips}}</h2>
+        <h2 class="title" style="font-size:2rem">{{this.Results}}</h2>
+ 
     </div>
 </template>
 
@@ -17,16 +27,61 @@
             
 
         },
-        async mounted(){
-            await fetch(`http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion/${this.$route.params.name}.json`).then((res) => res.json()).then(data => this.Results = data.data)
-        }
+        computed:{
+            getSplash: function() {
+                return `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${this.Results.id}_0.jpg`
+              
+            }
+        },
         
+
+        async mounted(){
+            await fetch(`http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion/${this.$route.params.name}.json`).then((res) => res.json()).then(data => this.Results = data.data)
+
+            this.Results = Object.values(this.Results)[0];
+            this.Results.title = this.Results.title.toUpperCase();
+            this.Results.name = this.Results.name.toUpperCase();
+     
+
+        }
+     
        
     }
 </script>
 
 <style  scoped>
-    .title{
-        font-size: 2rem;
+@import url(//db.onlinewebfonts.com/c/12420e8c141ca7c3dff41de2d59df13e?family=BeaufortforLOL-Bold);
+ 
+    .page{
+        background-color: rgb(0, 9, 19);
+        color: #FFFF;
+
     }
-</style>
+    .name{
+        text-align: center;
+        font-size: 8rem;
+        font-family: "BeaufortforLOL-Bold";
+        color: #FFFF;
+     
+  
+    }
+    .blurBG{
+        filter: blur(8px);
+       
+        z-index: 0;
+        margin: auto;
+        
+    }
+    .BG{
+        height: 100%;
+    }
+    .splashImg{
+    
+        position:absolute;
+        margin: auto;
+        z-index: 1;
+        width: 90%;
+      
+        
+    }
+</style>+

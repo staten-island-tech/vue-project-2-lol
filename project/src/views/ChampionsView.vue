98cacf55f157a16bf495d0f63402adc7d0862f91
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-05-08 03:01:05 -0400

    Created filter buttons

--- project/src/views/ChampionsView.vue
+++ project/src/views/ChampionsView.vue
@@ -4,20 +4,24 @@
       
         <form action="" class="searchForm">
             <input placeholder="Search Bar" type="text" class="searchBar" @input="updateSearchValue">
+
             <a href="https://www.youtube.com/watch?v=ek-672uRnhQ"><img src="https://freepngimg.com/download/league_of_legends/85646-and-league-legends-riot-icons-of-computer.png" alt="LOL Icon"></a>
         </form>
+        <div class="filter">
 
-
-         
-    
- 
+            <button class="filterB" @click="filterTank" >Tank</button>
+            <button class="filterB" @click="filterMage" >Mage</button>
+            <button class="filterB" @click="filterAss" >Assassin</button>
+        </div>
         
-        <div class="container" id="container">
+        <div   class="container" id="container">
+           
 
-            <div  class="thediv"  v-for="champion in searchFilter" :key="champion.name">
-                <ChampCard class="card"   :champid="champion.id"/>
-            
-            </div>
+                <div   class="thediv"  v-for="champion in searchFilter" :key="champion.name">
+                    <ChampCard class="card"   :champid="champion.id"/>
+                
+                </div>
+             
         </div>
         
     </div>
@@ -32,6 +36,8 @@ import ChampCard from "../components/ChampCard.vue"
         setup(){
             const store = useStore();
             const DataList = store.state.ChampList
+        
+         
             return{
            
                 store,DataList,
@@ -49,19 +55,60 @@ import ChampCard from "../components/ChampCard.vue"
             },
             updateSearchValue (value){
                 this.store.commit('searchedChamp', value.target.value.replace(/\s/g, ''));
+                 
              
             },
-            
-         
+             
+
+
+            filterTank: function(){
+                this.DataList = this.store.state.ChampList
+                this.DataList = this.DataList.filter((champ) => champ.tags.includes("Tank")  )
+                if(this.store.state.searchValue.includes(" ")){
+                    this.store.state.searchValue = this.store.state.searchValue.trim()
+                }
+                    this.store.state.searchValue = this.store.state.searchValue + " "
+               
+                
+            },
+            filterMage: function(){
+                this.DataList = this.store.state.ChampList
+                this.DataList = this.DataList.filter((champ) => champ.tags.includes("Mage")  )
+                if(this.store.state.searchValue.includes(" ")){
+                    this.store.state.searchValue = this.store.state.searchValue.trim()
+                }
+                    this.store.state.searchValue = this.store.state.searchValue + " "
+                
+            },
+            filterAss: function(){
+                this.DataList = this.store.state.ChampList
+                this.DataList = this.DataList.filter((champ) => champ.tags.includes("Assassin")  )
+                if(this.store.state.searchValue.includes(" ")){
+                    this.store.state.searchValue = this.store.state.searchValue.trim()
+                }
+                    this.store.state.searchValue = this.store.state.searchValue + " "
+              
+                
+            },
         },
         computed:{
             searchFilter: function(){
-                return this.DataList.filter((champion) => {
-                    console.log(this.store.state.searchValue.length)
-                    return champion.id.toLowerCase().match(this.store.state.searchValue.toLowerCase())
+                try {
+                    return this.DataList.filter((champion) => {
+                    champion.name = champion.id + " "
+                    return champion.name.toLowerCase().match(this.store.state.searchValue.toLowerCase())
                 })
-            }
-        }
+                } catch (error) {
+                   return error
+                }
+                
+            },
+            
+         
+
+
+        },
+       
         
        
         
@@ -130,6 +177,27 @@ import ChampCard from "../components/ChampCard.vue"
     .thediv{
         width: 25%;
     }
-    
- 
-</style>
+    .filter{
+        
+        font-size: 4rem;
+        width: 80%;
+        margin: auto;
+        text-align: center;
+
+    }
+      
+    .filterB{
+        display: inline;
+        background-color: transparent;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        padding-left:1rem ;
+        padding-right: 1rem;
+        border-top: 0.5rem red solid;
+        border-bottom: 0.5rem red solid;
+    }
+    .filterB:hover{
+        border-top: 0.5rem blue solid;
+        border-bottom: 0.5rem blue solid;
+    }
+ </style>

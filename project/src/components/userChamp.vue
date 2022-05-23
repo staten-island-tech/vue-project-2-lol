a88298c916d5d931d4877b8e1e5bf5bd43b0d08c
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-05-23 13:11:38 -0400

    yo som eone st7yle card please

--- project/src/components/userChamp.vue
+++ project/src/components/userChamp.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
         <div class="card">
-            <img :src="imageURL" alt="">
-            <h1 class="name"> {{title}}  {{name}}</h1>
-            <h2>{{description}}</h2>
+            <div class="left">
+                <img :src="imageURL" alt="">
+
+            </div>
+
+            <div class="right">
+
+                <h1 class="name"> {{title}}  {{name}}</h1>
+                <h2>{{description}}</h2>
+
+            </div>
         </div>
     </div>
 </template>
@@ -21,19 +29,32 @@
 
 <style scoped>
     .card{
-        height: 35rem;
-        width: 25rem;
+        width: 35rem;
+        height: 25rem;
         font-size: 2rem;
         text-align: center;
         border: 1px solid red;
         background-color: lightblue;
         margin: 2rem 2rem 2rem 2rem;
         color:black;
+        padding-top: 2rem;
+        display: flex;
+  
+        
+      
      
  
     }
     img{
-        height: 150px;
-        width: 150px;
+        height: 100%;
+        width: 100%;
+    }
+    
+    .left{
+        width: 30%;
+
+    }
+    .right{
+        width: 70%;
     }
 </style>

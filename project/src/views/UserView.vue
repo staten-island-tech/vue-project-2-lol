66255fc98ab9d6b1a4570aedd3dec4c531a756fc
Yessir664 <justin.zhang23@sitechhs.com>
2022-05-26 10:42:44 -0400

    commit

--- project/src/views/UserView.vue
+++ project/src/views/UserView.vue
@@ -1,46 +1,49 @@
 <template>
-<div>
-
-  <div class="user">
-    <button
-      v-for="{ icon, level, name } in accountData"
-      :key="name"
-      v-on:click="
-        handleSubmit(name);
-        toHistory();
-      "
-      class="user-button"
-    >
-      <div>
-        <div>{{ name }}</div>
-        <div class="account-icon">
-          <img
-            :src="`https://opgg-static.akamaized.net/images/profile_icons/profileIcon${icon}.jpg?image=q_auto&image=q_auto,f_webp,w_auto&v=1652335123873`"
-          />
+  <div>
+    <div class="user">
+      <button
+        v-for="{ icon, level, name } in accountData"
+        :key="name"
+        v-on:click="
+          handleSubmit(name);
+          toHistory();
+        "
+        class="user-button"
+      >
+        <div>
+          <div>{{ name }}</div>
+          <div class="account-icon">
+            <img
+              :src="`https://opgg-static.akamaized.net/images/profile_icons/profileIcon${icon}.jpg?image=q_auto&image=q_auto,f_webp,w_auto&v=1652335123873`"
+            />
+          </div>
+          <div class="account-level">
+            <span>Level {{ level }}</span>
+          </div>
         </div>
-        <div class="account-level">
-          <span>Level {{ level }}</span>
-        </div>
-      </div>
-    </button>
-
-  
-  </div>
-      <h2 class="text">Your Champions</h2>
-    <div class="cards">
-      <user-champ-r v-for="champion in userC" :key="champion.champName" :imageURL="champion.imageURL"  :name="champion.champName" :title="champion.title" :description="champion.description" />
+      </button>
     </div>
-</div>
+    <h2 class="text">Your Champions</h2>
+    <div class="cards">
+      <user-champ-r
+        v-for="champion in userC"
+        :key="champion.champName"
+        :imageURL="champion.imageURL"
+        :name="champion.champName"
+        :title="champion.title"
+        :description="champion.description"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import { toRaw } from "vue";
 import { useStore } from "vuex";
-import userChampR from "../components/userChampR.vue"
+import userChampR from "../components/userChampR.vue";
 
 export default {
   setup() {
-    
     const store = useStore();
 
     const accountData = toRaw(store.state.accountData);
@@ -50,41 +53,37 @@ export default {
     const handleSubmit = async (name) => {
       store.commit("updateSummoner", name);
     };
-    const Champions = []
+    const Champions = [];
 
     return {
       store,
       accountData,
       handleSubmit,
-      Champions
+      Champions,
     };
   },
-  components:{
-    userChampR
+  components: {
+    userChampR,
   },
   methods: {
     toHistory: function () {
       this.$router.push({ name: "history" });
     },
- 
-    },
+  },
 
-  computed:{
-    userC: function(){
-      const list = []
-      for(const c in this.store.state.dbChamps){
-        if(this.store.state.dbChamps[c].author == this.store.state.user.uid){
-          list.push(this.store.state.dbChamps[c])
+  computed: {
+    userC: function () {
+      const list = [];
+      for (const c in this.store.state.dbChamps) {
+        if (this.store.state.dbChamps[c].author == this.store.state.user.uid) {
+          list.push(this.store.state.dbChamps[c]);
         }
       }
-      
-      return list
-    }
-  }
 
-  }
- 
-
+      return list;
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -95,7 +94,7 @@ export default {
   align-items: center;
   margin: 5rem;
 }
-.text{
+.text {
   font-size: 7rem;
   text-align: center;
 }
@@ -117,10 +116,10 @@ img {
   border-radius: 20rem;
   margin: 1rem;
 }
-.cards{
-  display:flex;
+.cards {
+  display: flex;
   width: 90%;
-  margin:auto;
+  margin: auto;
   flex-wrap: wrap;
   justify-content: space-evenly;
 }

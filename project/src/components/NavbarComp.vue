2c364b0584a276fb52f7c09dcfe92cbe7549dfc7
Yessir664 <67617753+Yessir664@users.noreply.github.com>
2022-05-30 22:51:18 -0400

    commit

--- project/src/components/NavbarComp.vue
+++ project/src/components/NavbarComp.vue
@@ -2,31 +2,37 @@
   <nav>
     <template v-if="authIsReady">
       <div class="navbar">
-       <router-link class="link" to="/">Home</router-link>
+        <router-link class="link" to="/">Home</router-link>
 
-      
-        <div class="link" v-if="user" > Logged in as {{user.email}}</div>
+        <div class="link" v-if="user">Logged in as {{ user.email }}</div>
 
-        <router-link class="link" v-if="!user" to="/signup">Sign Up</router-link>
+        <router-link class="link" v-if="!user" to="/signup"
+          >Sign Up</router-link
+        >
         <router-link class="link" v-if="!user" to="/login">Log In</router-link>
         <button v-if="user" class="logout" @click="handleClick">Logout</button>
 
         <div class="dropdown">
-          <button >Options</button>
+          <button>Options</button>
           <ul>
-            <li><router-link   class="link" to="/search">Search</router-link></li>
+            <li><router-link class="link" to="/search">Search</router-link></li>
 
-            <li><router-link class="link" to="/Champions">Champions</router-link></li>
-            
-            <li v-if="user"><router-link  class="link" to="user">User</router-link></li>
-            
-            <li v-if="user"><router-link class="link" to="/Create">Create</router-link></li>
+            <li>
+              <router-link class="link" to="/Champions">Champions</router-link>
+            </li>
+
+            <li v-if="user">
+              <router-link @click="readUsers" class="link" to="user"
+                >User</router-link
+              >
+            </li>
+
+            <li v-if="user">
+              <router-link class="link" to="/Create">Create</router-link>
+            </li>
           </ul>
         </div>
-
-      </div> 
-
-
+      </div>
 
       <!-- <div class="navbar">
         <div class="left">
@@ -76,34 +82,30 @@ export default {
 };
 </script>
 <style scoped>
-html, body, * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-size: 62.5%;
-    font-family: "Roboto", sans-serif;
+html,
+body,
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 62.5%;
+  font-family: "Roboto", sans-serif;
 }
- 
-.navbar{
-   
+
+.navbar {
   width: 100%;
   background-color: lightcoral;
-   
+
   display: flex;
   justify-content: space-around;
   align-items: center;
   font-size: 10em;
   height: 5rem;
-  
-  
- 
-  align-items: center;
-  
 
+  align-items: center;
 }
 
-
- .dropdown ul{
+.dropdown ul {
   position: absolute;
   z-index: 2;
   background-color: white;
@@ -112,7 +114,7 @@ html, body, * {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  align-items: center ;
+  align-items: center;
   list-style: none;
   opacity: 0;
   pointer-events: none;
@@ -120,8 +122,7 @@ html, body, * {
   transform: translateY(-10px);
   border-radius: 5px;
 }
-.dropdown li{
-
+.dropdown li {
   width: 100%;
   height: 100%;
   display: flex;
@@ -130,25 +131,23 @@ html, body, * {
   border-radius: 5px;
 }
 
-button{
+button {
   border: none;
   background-color: transparent;
   font-size: 3rem;
 }
-.dropdown li:hover{
+.dropdown li:hover {
   background-color: lightyellow;
   pointer-events: all;
 }
-.dropdown button:focus + ul{
-  opacity:  1;
+.dropdown button:focus + ul {
+  opacity: 1;
   pointer-events: all;
-  transform: translateY(0px
-  );
+  transform: translateY(0px);
 }
-.link{
+.link {
   text-decoration: none;
   color: black;
   font-size: 3rem;
-  
 }
 </style>
